c28ec9d857f47cba3019997bb83500d8ecd8dad5
Taylor Grote <grotese@gmail.com>
2016-06-28 20:10:14 -0500

    Add Script folder and Vector2i

---
+++ Assets/Scripts/Vector2i.cs
@@ -0,0 +1,21 @@
+﻿public struct Vector2i
+{
+	public int x, y;
+
+	public Vector2i( int x, int y )
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	// Get that Hash Bro
+	public override int GetHashCode()
+	{
+		int hash = 17;
+
+		hash = hash * 23 + x.GetHashCode();
+		hash = hash * 23 + y.GetHashCode();
+
+		return hash;    // blaze it, lol ;)
+	}
+}
