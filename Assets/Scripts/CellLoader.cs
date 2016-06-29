5c73a14e51f2962fc8b4bb910a2d4bf9582bd223
Taylor Grote <grotese@gmail.com>
2016-06-28 20:10:38 -0500

    Add CellLoader Script

---
+++ Assets/Scripts/CellLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The purpose of this script tis to monitor the player's movement.  If the
+// player moves beyond the bounds of a cell, then evaluate if we need to load
+// another cell and unload a cell behind him.
+public class CellLoader : MonoBehaviour 
+{
+}
