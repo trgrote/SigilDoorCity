bfe6909fc61b072dca87f5bf4d583e10e5eee9e2
Taylor Grote <grotese@gmail.com>
2016-07-03 17:51:31 -0500

    Add Vector3 Extensions

---
+++ Assets/Scripts/Vector3Extensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class Vector3Extensions
+{
+	// Return INverse of vector
+	static public Vector3 Inverse( this Vector3 vec )
+	{
+		return new Vector3( -vec.x, -vec.y, -vec.z );
+	}
+
+	// Invert the vector ( modifes vector )
+	static public void Invert( this Vector3 vec )
+	{
+		vec.x = -vec.x;
+		vec.y = -vec.y;
+		vec.z = -vec.z;
+	}
+
+}
